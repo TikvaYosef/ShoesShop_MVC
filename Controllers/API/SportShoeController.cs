5af0f3cfb795f017be74bfab1a0753565834164b
TikvaYosef <tikvayosef13@gmail.com>
2022-01-16 21:18:05 +0200

    Add project files.

---
+++ Controllers/API/SportShoeController.cs
@@ -0,0 +1,48 @@
+﻿using ShoesShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoesShop.Controllers.API
+{
+    public class SportShoeController : Controller
+    {
+        SportShoe[] arrayOfShoes = new SportShoe[]{
+        new SportShoe(0,"nike","air",37,350),
+        new SportShoe(1,"balnciaga","nfdk",41,450),
+        new SportShoe(2,"jorden","rghy56",36,400),
+        new SportShoe(3,"kljhj","fhtf3",42,250)
+
+        };
+        public ActionResult Showbrand()
+        {
+           
+                ViewBag.firstBrand = arrayOfShoes[0].brand;
+                return View();
+        }
+
+       
+        public ActionResult ShowShoe(int id)
+        {
+            foreach (SportShoe item in arrayOfShoes)
+            {
+                if (id == item.id)
+                {
+                    ViewBag.shoe = item;
+                }
+            }
+            return View(ViewBag.shoe);
+        }
+
+       
+        public ActionResult ShowAllShoes()
+        {
+
+            return View(arrayOfShoes);
+        }
+
+     
+    }
+}
---
+++ Models/SportShoe.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShoesShop.Models
+{
+    public class SportShoe
+    {
+        public int id;
+        public string company;
+        public string brand;
+        public int size;
+        public int price;
+
+        public SportShoe(int id, string company, string brand, int size, int price)
+        {
+            this.id = id;
+            this.company = company;
+            this.brand = brand;
+            this.size = size;
+            this.price = price;
+        }
+    }
+}
